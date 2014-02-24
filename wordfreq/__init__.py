c7c8078883f7c50e458500022ec6e6ec437358d8
Rob Speer <rob@luminoso.com>
2014-02-24 18:03:31 -0500

    A different plan for the top-level word_frequency function.
    When, before, I was importing wordfreq.query at the top level, this
    created a dependency loop when installing wordfreq.
    
    The new top-level __init__.py provides just a `word_frequency` function,
    which imports the real function as needed and calls it. This should
    avoid the dependency loop, at the cost of making
    `wordfreq.word_frequency` slightly less efficient than
    `wordfreq.query.word_frequency`.
    
    
    Former-commit-id: 44ccf40742fc7e13e910e418ea5c43f3ee72dcb2

--- wordfreq/__init__.py
+++ wordfreq/__init__.py
@@ -1,2 +1,16 @@
-# Import some methods from wordfreq.query at the top level.
-from wordfreq.query import word_frequency, iter_wordlist, random_words
+# Make wordfreq.query available at the top level when needed.
+
+def word_frequency(word, lang, wordlist='multi', offset=0.):
+    """
+    Get the frequency of `word` in the language with code `lang`, from the
+    specified `wordlist`.
+
+    The offset gets added to all values, to monotonically account for the
+    fact that we have not observed all possible words.
+
+    This is a wrapper for the real word_frequency function, so that it can
+    be imported at the top level instead of from `wordfreq.query`.
+    """
+    from wordfreq.query import word_frequency as _real_word_frequency
+    return _real_word_frequency(word, lang, wordlist=wordlist, offset=offset)
+

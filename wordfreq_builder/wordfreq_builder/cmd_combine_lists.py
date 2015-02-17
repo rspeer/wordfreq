bc780c63c8a7a6d8a5b875068cfa0159b4fa4324
Robyn Speer <rspeer@luminoso.com>
2015-02-17 13:43:22 -0500

    enable wordlist balancing, surface form counting

--- wordfreq_builder/wordfreq_builder/cmd_combine_lists.py
+++ wordfreq_builder/wordfreq_builder/cmd_combine_lists.py
@@ -3,18 +3,19 @@ from pathlib import Path
 import argparse
 
 
-def merge_lists(input_names, output_name):
+def merge_lists(input_names, output_name, balance=False):
     count_dicts = []
     for input_name in input_names:
         count_dicts.append(read_counts(Path(input_name)))
-    merged = merge_counts(count_dicts)
+    merged = merge_counts(count_dicts, balance=balance)
     write_counts(merged, Path(output_name))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output', help='filename to write the output to', default='combined-counts.csv')
+    parser.add_argument('-b', '--balance', action='store_true', help='Automatically balance unequally-sampled word frequencies')
     parser.add_argument('inputs', help='names of input files to merge', nargs='+')
     args = parser.parse_args()
-    merge_lists(args.inputs, args.output)
+    merge_lists(args.inputs, args.output, balance=args.balance)
 

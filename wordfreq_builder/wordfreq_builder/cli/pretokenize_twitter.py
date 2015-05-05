59409266cad1994cdc4db2242291ad5b72e636ee
Robyn Speer <rspeer@luminoso.com>
2015-05-05 13:59:21 -0400

    add and adjust some build steps
    - more build steps for Wikipedia
    - rename 'tokenize_twitter' to 'pretokenize_twitter' to indicate that
      the results are preliminary

--- wordfreq_builder/wordfreq_builder/cli/pretokenize_twitter.py
+++ wordfreq_builder/wordfreq_builder/cli/pretokenize_twitter.py
@@ -1,10 +1,10 @@
-from wordfreq_builder.tokenizers import rosette_surface_tokenizer, tokenize_file
+from wordfreq_builder.tokenizers import rosette_surface_tokenizer, pretokenize_file
 import argparse
 
 
-def tokenize_twitter(in_filename, out_prefix):
-    tokenize_file(in_filename, out_prefix,
-                  tokenizer=rosette_surface_tokenizer)
+def pretokenize_twitter(in_filename, out_prefix):
+    pretokenize_file(in_filename, out_prefix,
+                     tokenizer=rosette_surface_tokenizer)
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
     parser.add_argument('filename', help='filename of input file containing one tweet per line')
     parser.add_argument('outprefix', help='prefix of output filenames')
     args = parser.parse_args()
-    tokenize_twitter(args.filename, args.outprefix)
+    pretokenize_twitter(args.filename, args.outprefix)
 
 
 if __name__ == '__main__':

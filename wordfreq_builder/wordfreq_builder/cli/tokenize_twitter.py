80afc5dc453d13ec7db4f32b646d6a3e0f989932
Joshua Chin <jchin@luminoso.com>
2015-06-16 17:26:52 -0400

    renamed pretokenize_twitter to tokenize twitter, and deleted format_twitter

--- wordfreq_builder/wordfreq_builder/cli/tokenize_twitter.py
+++ wordfreq_builder/wordfreq_builder/cli/tokenize_twitter.py
@@ -1,9 +1,9 @@
-from wordfreq_builder.tokenizers import cld2_surface_tokenizer, pretokenize_file
+from wordfreq_builder.tokenizers import cld2_surface_tokenizer, tokenize_file
 import argparse
 
 
-def pretokenize_twitter(in_filename, out_prefix):
-    pretokenize_file(in_filename, out_prefix,
+def tokenize_twitter(in_filename, out_prefix):
+    tokenize_file(in_filename, out_prefix,
                      tokenizer=cld2_surface_tokenizer)
 
 
@@ -12,7 +12,7 @@ def main():
     parser.add_argument('filename', help='filename of input file containing one tweet per line')
     parser.add_argument('outprefix', help='prefix of output filenames')
     args = parser.parse_args()
-    pretokenize_twitter(args.filename, args.outprefix)
+    tokenize_twitter(args.filename, args.outprefix)
 
 
 if __name__ == '__main__':

2d58ba94f21cbec61a8a81007f4c60d4656e8fc6
Rob Speer <rob@luminoso.com>
2015-09-03 18:10:54 -0400

    Add SUBTLEX as a source of English and Chinese data
    Meanwhile, fix up the dependency graph thingy. It's actually kind of
    legible now.

--- wordfreq_builder/wordfreq_builder/cli/merge_freqs.py
+++ wordfreq_builder/wordfreq_builder/cli/merge_freqs.py
@@ -2,18 +2,19 @@ from wordfreq_builder.word_counts import read_freqs, merge_freqs, write_wordlist
 import argparse
 
 
-def merge_lists(input_names, output_name):
+def merge_lists(input_names, output_name, cutoff):
     freq_dicts = []
     for input_name in input_names:
-        freq_dicts.append(read_freqs(input_name, cutoff=2))
+        freq_dicts.append(read_freqs(input_name, cutoff=cutoff))
     merged = merge_freqs(freq_dicts)
     write_wordlist(merged, output_name)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', help='filename to write the output to', default='combined-counts.csv')
+    parser.add_argument('-o', '--output', help='filename to write the output to', default='combined-freqs.csv')
+    parser.add_argument('-c', '--cutoff', type=int, help='stop after seeing a count below this', default=2)
     parser.add_argument('inputs', help='names of input files to merge', nargs='+')
     args = parser.parse_args()
-    merge_lists(args.inputs, args.output)
+    merge_lists(args.inputs, args.output, args.cutoff)
 

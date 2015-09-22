6cf4210187430c822297ce921775db6d983c22ab
Rob Speer <rob@luminoso.com>
2015-09-22 16:31:50 -0400

    load the Chinese character mapping from a .msgpack.gz file

--- wordfreq/chinese.py
+++ wordfreq/chinese.py
@@ -1,14 +1,19 @@
 from pkg_resources import resource_filename
-from wordfreq._chinese_mapping import SIMPLIFIED_MAP
 import jieba
-
+import msgpack
+import gzip
 
 jieba_tokenizer = None
+simplified_map = None
 DICT_FILENAME = resource_filename('wordfreq', 'data/jieba_zh.txt')
+SIMP_MAP_FILENAME = resource_filename('wordfreq', 'data/_chinese_mapping.msgpack.gz')
 
 
 def simplify_chinese(text):
-    return text.translate(SIMPLIFIED_MAP).casefold()
+    global simplified_map
+    if simplified_map is None:
+        simplified_map = msgpack.load(gzip.open(SIMP_MAP_FILENAME), encoding='utf-8')
+    return text.translate(simplified_map).casefold()
 
 
 def jieba_tokenize(text):

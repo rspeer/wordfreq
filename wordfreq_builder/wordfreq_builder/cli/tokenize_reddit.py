8d9668d8ab8118c97880a5be6c43e6d4fd04b367
Rob Speer <rob@luminoso.com>
2016-01-12 13:05:38 -0500

    fix usage text: one comment, not one tweet

--- wordfreq_builder/wordfreq_builder/cli/tokenize_reddit.py
+++ wordfreq_builder/wordfreq_builder/cli/tokenize_reddit.py
@@ -4,7 +4,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help='filename of input file containing one tweet per line')
+    parser.add_argument('filename', help='filename of input file containing one comment per line')
     parser.add_argument('outprefix', help='prefix of output filenames')
     args = parser.parse_args()
     tokenize_by_language(args.filename, args.outprefix, tokenizer=cld2_reddit_tokenizer)

56f830d678eaea94bd1d8dbe6914a1b6167a4fb4
Rob Speer <rob@luminoso.com>
2016-01-13 12:05:07 -0500

    reformat some argparse argument definitions
    Former-commit-id: 511fcb6f91fb9a1c3c3a22ca07dd7bd96e21cb82

--- wordfreq_builder/wordfreq_builder/cli/merge_counts.py
+++ wordfreq_builder/wordfreq_builder/cli/merge_counts.py
@@ -13,9 +13,12 @@ def merge_lists(input_names, output_name, cutoff=0):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', help='filename to write the output to', default='combined-counts.csv')
-    parser.add_argument('-c', '--cutoff', type=int, default=0, help='minimum count to read from an input file')
-    parser.add_argument('inputs', help='names of input files to merge', nargs='+')
+    parser.add_argument('-o', '--output', default='combined-counts.csv',
+                        help='filename to write the output to')
+    parser.add_argument('-c', '--cutoff', type=int, default=0
+                        help='minimum count to read from an input file')
+    parser.add_argument('inputs', nargs='+',
+                        help='names of input files to merge')
     args = parser.parse_args()
     merge_lists(args.inputs, args.output, cutoff=args.cutoff)
 

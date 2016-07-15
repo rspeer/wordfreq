0a2bfb27103aaba1471d6a0c02ad6ce9260ef55b
Robyn Speer <rspeer@luminoso.com>
2016-07-15 15:10:25 -0400

    Tokenization in Korean, plus abjad languages (#38)
    * Remove marks from more languages
    
    * Add Korean tokenization, and include MeCab files in data
    
    * add a Hebrew tokenization test
    
    * fix terminology in docstrings about abjad scripts
    
    * combine Japanese and Korean tokenization into the same function
    
    
    Former-commit-id: fec6eddcc3475f49a7541d8d3202ec87e581ed53

---
+++ wordfreq/mecab.py
@@ -0,0 +1,28 @@
+from pkg_resources import resource_filename
+import MeCab
+import unicodedata
+
+
+# Instantiate the MeCab analyzers for each language.
+MECAB_ANALYZERS = {
+    'ja': MeCab.Tagger('-d %s' % resource_filename('wordfreq', 'data/mecab-ja-ipadic')),
+    'ko': MeCab.Tagger('-d %s' % resource_filename('wordfreq', 'data/mecab-ko-dic'))
+}
+
+
+def mecab_tokenize(text, lang):
+    """
+    Use the mecab-python3 package to tokenize the given text. The `lang`
+    must be 'ja' for Japanese or 'ko' for Korean.
+
+    The simplest output from mecab-python3 is the single-string form, which
+    contains the same table that the command-line version of MeCab would output.
+    We find the tokens in the first column of this table.
+    """
+    if lang not in MECAB_ANALYZERS:
+        raise ValueError("Can't run MeCab on language %r" % lang)
+    analyzer = MECAB_ANALYZERS[lang]
+    text = unicodedata.normalize('NFKC', text.strip())
+    return [line.split('\t')[0]
+            for line in analyzer.parse(text).split('\n')
+            if line != '' and line != 'EOS']

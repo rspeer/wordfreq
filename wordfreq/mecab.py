c1927732d397c749fb0a6b80345683ebaa944330
Rob Speer <rob@luminoso.com>
2016-07-29 17:27:15 -0400

    Look for MeCab dictionaries in various places besides this package
    Former-commit-id: afe65379948fbf2fc4603c2bc9e620a3d55c1957

--- wordfreq/mecab.py
+++ wordfreq/mecab.py
@@ -1,12 +1,56 @@
 from pkg_resources import resource_filename
 import MeCab
 import unicodedata
+import os
+
+
+def find_mecab_dictionary(names):
+    """
+    Find a MeCab dictionary with a given name. The dictionary might come as
+    part of this repository (if you got wordfreq from GitHub) or might have to
+    be installed separately (if you got wordfreq from PyPI).
+
+    We'd prefer to include MeCab in the repository all the time, but PyPI's
+    package size limits make that not an option.
+    """
+    suggested_pkg = names[0]
+    paths = [
+        resource_filename('wordfreq', 'data'),
+        os.path.expanduser('~/.local/lib/mecab/dic'),
+        '/var/lib/mecab/dic',
+        '/var/local/lib/mecab/dic',
+        '/usr/lib/mecab/dic',
+        '/usr/local/lib/mecab/dic',
+    ]
+    full_paths = [os.path.join(path, name) for path in paths for name in names]
+    for path in full_paths:
+        if os.path.exists(path):
+            return path
+
+    error_lines = [
+        "Couldn't find the MeCab dictionary named %r." % name,
+        "You should download or use your system's package manager to install",
+        "the %r package." % suggested_pkg,
+        "",
+        "We looked in the following locations:"
+    ] + ["\t%s" % path for path in full_paths]
+
+    raise OSError('\n'.join(error_lines))
+
+
+def make_mecab_analyzer(names):
+    """
+    Get a MeCab analyzer object, given a list of names the dictionary might
+    have.
+    """
+    filename = find_mecab_dictionary(names)
+    return MeCab.Tagger('-d %s' % filename)
 
 
 # Instantiate the MeCab analyzers for each language.
 MECAB_ANALYZERS = {
-    'ja': MeCab.Tagger('-d %s' % resource_filename('wordfreq', 'data/mecab-ja-ipadic')),
-    'ko': MeCab.Tagger('-d %s' % resource_filename('wordfreq', 'data/mecab-ko-dic'))
+    'ja': make_mecab_analyzer(['mecab-ipadic-utf8', 'mecab-ja-ipadic', 'ipadic-utf8']),
+    'ko': make_mecab_analyzer(['mecab-ko-dic', 'ko-dic'])
 }
 
 

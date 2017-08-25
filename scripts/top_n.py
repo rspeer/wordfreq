e3352392cc6404ed2140a49335d11109abb55371
Rob Speer <rob@luminoso.com>
2017-08-25 17:37:48 -0400

    v1.7: update tokenization, update data, add `bn` and `mk`

---
+++ scripts/top_n.py
@@ -0,0 +1,14 @@
+"""
+A quick script to output the top N words (1000 for now) in each language.
+You can send the output to a file and diff it to see changes between wordfreq
+versions.
+"""
+import wordfreq
+
+
+N = 1000
+
+
+for lang in sorted(wordfreq.available_languages()):
+    for (i, word) in enumerate(wordfreq.top_n_list(lang, 1000)):
+        print('{}\t{}'.format(lang, word))

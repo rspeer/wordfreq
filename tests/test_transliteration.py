fe85b4e124c0158619092f62b0087c9784638651
Robyn Speer <rspeer@luminoso.com>
2018-03-08 16:47:36 -0500

    fix az-Latn transliteration, and test

--- tests/test_transliteration.py
+++ tests/test_transliteration.py
@@ -1,5 +1,6 @@
 from nose.tools import eq_
 from wordfreq import tokenize
+from wordfreq.preprocess import preprocess_text
 
 
 def test_transliteration():
@@ -9,6 +10,21 @@ def test_transliteration():
         ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš'])
     eq_(tokenize("Pa, ima tu mnogo stvari koje ne shvataš.", 'sr'),
         ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš'])
+
+    # I don't have examples of complete sentences in Azerbaijani that are
+    # naturally in Cyrillic, because it turns out everyone writes Azerbaijani
+    # in Latin letters on the Internet, _except_ sometimes for Wiktionary.
+    # So here are some individual words.
+
+    # 'library' in Azerbaijani Cyrillic
+    eq_(preprocess_text('китабхана', 'az'), 'kitabxana')
+    eq_(preprocess_text('КИТАБХАНА', 'az'), 'kitabxana')
+    eq_(preprocess_text('KİTABXANA', 'az'), 'kitabxana')
+
+    # 'scream' in Azerbaijani Cyrillic
+    eq_(preprocess_text('бағырты', 'az'), 'bağırtı')
+    eq_(preprocess_text('БАҒЫРТЫ', 'az'), 'bağırtı')
+    eq_(preprocess_text('BAĞIRTI', 'az'), 'bağırtı')
 
 
 def test_actually_russian():

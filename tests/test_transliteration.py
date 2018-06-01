96a01b96859a915736d45f7f6298e42d52774efe
Rob Speer <rob@luminoso.com>
2018-06-01 16:40:51 -0400

    port remaining tests to pytest

--- tests/test_transliteration.py
+++ tests/test_transliteration.py
@@ -1,15 +1,18 @@
-from nose.tools import eq_
 from wordfreq import tokenize
 from wordfreq.preprocess import preprocess_text
 
 
 def test_transliteration():
     # "Well, there's a lot of things you do not understand."
-    # (from somewhere in OpenSubtitles)
-    eq_(tokenize("Па, има ту много ствари које не схваташ.", 'sr'),
-        ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš'])
-    eq_(tokenize("Pa, ima tu mnogo stvari koje ne shvataš.", 'sr'),
-        ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš'])
+    # (from somewhere in OpenSubtitles
+    assert (
+        tokenize("Па, има ту много ствари које не схваташ.", 'sr') ==
+        ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš']
+    )
+    assert (
+        tokenize("Pa, ima tu mnogo stvari koje ne shvataš.", 'sr') ==
+        ['pa', 'ima', 'tu', 'mnogo', 'stvari', 'koje', 'ne', 'shvataš']
+    )
 
     # I don't have examples of complete sentences in Azerbaijani that are
     # naturally in Cyrillic, because it turns out everyone writes Azerbaijani
@@ -17,14 +20,14 @@ def test_transliteration():
     # So here are some individual words.
 
     # 'library' in Azerbaijani Cyrillic
-    eq_(preprocess_text('китабхана', 'az'), 'kitabxana')
-    eq_(preprocess_text('КИТАБХАНА', 'az'), 'kitabxana')
-    eq_(preprocess_text('KİTABXANA', 'az'), 'kitabxana')
+    assert preprocess_text('китабхана', 'az') == 'kitabxana'
+    assert preprocess_text('КИТАБХАНА', 'az') == 'kitabxana'
+    assert preprocess_text('KİTABXANA', 'az') == 'kitabxana'
 
     # 'scream' in Azerbaijani Cyrillic
-    eq_(preprocess_text('бағырты', 'az'), 'bağırtı')
-    eq_(preprocess_text('БАҒЫРТЫ', 'az'), 'bağırtı')
-    eq_(preprocess_text('BAĞIRTI', 'az'), 'bağırtı')
+    assert preprocess_text('бағырты', 'az') == 'bağırtı'
+    assert preprocess_text('БАҒЫРТЫ', 'az') == 'bağırtı'
+    assert preprocess_text('BAĞIRTI', 'az') == 'bağırtı'
 
 
 def test_actually_russian():
@@ -35,15 +38,13 @@ def test_actually_russian():
     # We make sure to handle this case so we don't end up with a mixed-script
     # word like "pacanы".
 
-    eq_(tokenize("сто из ста, пацаны!", 'sr'),
-        ['sto', 'iz', 'sta', 'pacany'])
-
-    eq_(tokenize("культуры", 'sr'), ["kul'tury"])
+    assert tokenize("сто из ста, пацаны!", 'sr') == ['sto', 'iz', 'sta', 'pacany']
+    assert tokenize("культуры", 'sr') == ["kul'tury"]
 
 
 def test_alternate_codes():
     # Try language codes for Serbo-Croatian that have been split, and now
     # are canonically mapped to Serbian
-    eq_(tokenize("культуры", 'sh'), ["kul'tury"])
-    eq_(tokenize("культуры", 'hbs'), ["kul'tury"])
+    assert tokenize("культуры", 'sh') == ["kul'tury"]
+    assert tokenize("культуры", 'hbs') == ["kul'tury"]
 
